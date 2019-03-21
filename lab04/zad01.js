a10b970a7d320d1d13b8c5c762e44291ef0bd2b5
Antonina Brzeska <aformela2@sigma.ug.edu.pl>
2019-03-21 13:03:01 +0100

    edit zad01

--- lab04/zad01.js
+++ lab04/zad01.js
@@ -4,23 +4,57 @@
 function multiply(p1, p2) {
     return p1 * p2;  
   }
-function defFun(fun, types){
+  var defFun = function (fun, types) {
     fun.typeConstr = types;
     return fun;
-}
-function defFun(){}
-
-    //console.log(defFun(multiply(2,3), ['number','number']));
-  //const myfun = defFun((a, b) => a + b, ['number', 'number']);
+  };
+  
+  var testFun = defFun(function (a, b) {
+    return a + b;
+  }, ['number', 'number']);
+  
+  // console.log(testFun);
+  // console.log(testFun(2, 3));
+  //console.log(testFun.typeConstr);
+  //
+  // console.log("\n////////////////////////\n");
+  
+  var appFun = function (f) {
+    if (f.typeConstr === 'undefined') {
+      throw({ typerr: "typeConstr niezdefiniowany" });
+    }
+    // rozwiazanie ze slice:
+    // let args = Array.prototype.slice.call(arguments, 1);
+    // for (let i = 0; i < args.length; i++) {
+    //   if (typeof args[i] !== f.typeConstr[i]) {
+    //     let errorMsg = "Type of arg" + i + " ('" + typeof args[i] + "')" +
+    //       " does not match typeConstr[" + i + "] ('" + f.typeConstr[i] + "')";
+    //     throw({ typerr: errorMsg });
+    //   }
+    // }
+  
+    // using basic Array operations
+  
+    let args = [];
+    for (let i = 1; i < arguments.length; i++) {
+      if (typeof arguments[i] !== f.typeConstr[i-1]) {
+        let errorMsg = "Type of arg" + (i-1) + " ('" + typeof arguments[i] + "')" +
+          " does not match typeConstr[" + (i-1) + "] ('" + f.typeConstr[i-1] + "')";
+        throw({ typerr: errorMsg });
+      }
+      args.push(arguments[i]);
+    }
+    return f.apply(this, args);
+  };
+  
+  try {
+    console.log(appFun(testFun, 2, "3"));
+  } catch (e) {
+    console.log(e.typerr);
+  }
 
   try {
     console.log(multiply(3, 4));
-} catch (e) {
-    console.log(e.typerr);
-}
-
-var appFun = function (f) {
-
-    //let args = [];
-    //for (let i = 1; i < arguments.length; i++) {}
-}
+    } catch (e) {
+        console.log(e.typerr);
+    }
