397f9db14d3975d7e1d576fcfb2ba784728ba2da
Linras <linras@wp.pl>
2019-05-30 22:27:14 +0200

    Fixing mastermind.

--- lab07/app.js
+++ lab07/app.js
@@ -1,12 +1,15 @@
 // jshint esversion: 6, node: true
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const uuidv1 = require('uuid/v1');  //do tworzenia id
+const uuid = require('uuid/v1');  //do tworzenia id
 const cookieParser = require('cookie-parser');
-const routes = require('./routes');
-const router = express.Router();
 
+const morgan = require('morgan')
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
 // bodyParser parsuje formaty:
 //   json, x-www-form-urlencoded i multipart/form-data
@@ -14,36 +17,60 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 
-var path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-//ejs sample
-app.get('/test2', routes.index('wymiata'));
 //app.get('/test3', routes.move());
 
-function getMarks(W, Z){
-    ret = {};
-    black=0;
-    white=0;
-    W.forEach((elW, indexW) => {
-        if (W[indexW]===Z[indexW]) {
-            black++;
-        }
-        else{
-            for (let indexZ = 0; indexZ < Z.length; indexZ++) {
-                const element = Z[indexZ];
-                if (Z[indexZ]==elW && elW!=W[indexZ]) {
-                    white++;
-                    break;  //in foreach break impossible
+//Zapamietywanie Gier
+const gamesMap = new Map();
+
+class Game {
+
+    constructor(gameId, size, colors, steps, solution) {
+        this.gameId = gameId;
+        this.size = size;
+        this.colors = colors;
+        this.steps = steps;
+        this.solution = solution;
+        this.kod = [];
+    }
+
+    generate() {
+        for (let i = 0; i < this.size; i++) {
+            this.kod.push(Math.floor(Math.random() * this.colors) + "");
+        };
+    }
+
+    getMarks(W){
+        let ret = {
+            white: 0,
+            black: 0
+        };
+        black=0;
+        white=0;
+        W.forEach((elW, indexW) => {
+            if (W[indexW]===solution[indexW]) {
+                black++;
+            }
+            else{
+                for (let indexZ = 0; indexZ < solution.length; indexZ++) {
+                    const element = solution[indexZ];
+                    if (solution[indexZ]==elW && elW!=W[indexZ]) {
+                        white++;
+                        break;  //in foreach break impossible
+                    }
                 }
             }
-        }
-    });
-    ret.white=white;
-    ret.black=black;
-    return ret;
-};
+        });
+        ret.white=white;
+        ret.black=black;
+        return ret;
+    };
+
+
+
+}
 
 // const ind = require('./routes/index.js');
 // app.use('/test2',ind);
@@ -56,13 +83,10 @@ app.get('/test', (_req, res) => {
     });
   });
 
-app.get('/', (_req, res) => {
-    res.send('Dziala GET');
-  });
 
 // app.post('/game/new', (req, res) => {
 //     res.json({
-//         game: uuidv1(), //generate id
+//         game: uuid(), //generate id
 //         size: req.body.size,
 //         colors: req.body.colors,
 //         steps: 10
@@ -71,20 +95,57 @@ app.get('/', (_req, res) => {
 // });
 
 app.post('/game/move', (req, res) =>{
-    
-    ret=getMarks([1, 2, 5, 4, 1],[6,3,5,2,1]);  //TESTOWE
+    let Game = gamesMap.get(req.body.Game);
+    Game.steps--;
+
+    if (Game.steps == 0) {
+        Game.solution = 'over';
+    }
+
     res.json({
-        game: req.body.game,
-        result:{
-            black: ret.black,
-            white: ret.white
-        }
+        Game: req.body.Game,
+        result: Game.getMarks(req.body.move),
+        solution: Game.solution
     });
 });
 
-app.post('/game/move', routes.move);
-app.post('/game/new', routes.newGame);
-app.post('/s/', routes.test);
+
+// STATUS OF THE GAME
+app.post(('/game/status'), (req, res) => {
+    let Game = gamesMap.get(req.body.Game);
+
+    res.json({
+        Game: req.body.Game,
+        solution: Game.solution
+    });
+});
+
+
+//app.post('/game/move', routes.move);
+
+
+//NEW GAME
+app.post(('/game/new'), (req, res) => {
+
+    let Game = new Game(uuid(), req.body.size, req.body.colors, req.body.steps, false)
+    Game.generate();
+    gamesMap.set(Game.gameId, Game);
+
+    res.json({
+        Game: Game.gameId,
+        size: Game.size,
+        colors: Game.colors,
+        steps: Game.steps
+    });
+});
+
+
+//app.post('/s/', routes.test);
+
+// INIT
+app.get('/', function (req, res) {
+    res.sendFile("index.html");
+});
 
 app.listen(3000, () =>{
     console.log("Serwer dziala na porcie 3000");
--- lab07/public/script.js
+++ lab07/public/script.js
@@ -1,2 +1,232 @@
-/* jshint esversion: 6, browser: true, jquery: true, devel: true */
-/* globals axios: false, axiosJsonpAdapter:false */
+//Dane gry
+let gameId;
+let gameSize;
+let gameColors;
+let gameSteps;
+let step = 0;
+let result;
+
+
+const menuSize = document.getElementById('menu-size');
+const menuColors = document.getElementById('menu-colors');
+const menuStep = document.getElementById('menu-step');
+const uwagaSize = document.getElementById('uwaga-size');
+const uwagaColors = document.getElementById('uwaga-colors');
+const uwagaStep = document.getElementById('uwaga-step');
+const uwaga = document.getElementById('uwaga');
+const btnStart = document.getElementById('menu-start');
+const btnPlay = document.getElementById('play');
+const colors = document.getElementById('game-colors');
+const game = document.getElementById('game');
+const przebieg = document.getElementById('przebieg');
+
+//Walidacja 
+/*
+menuRozmiar.addEventListener('input', (e) => {
+    if (e.target.value <= 0 || e.target.value === '') {
+        uwagaRozmiar.textContent = 'Pole wymagane i wiekszę od 0';
+    } else {
+        uwagaRozmiar.textContent = '';
+    }
+    8
+});
+
+menuKolory.addEventListener('input', (e) => {
+    if (e.target.value <= 0 || e.target.value === '') {
+        uwagaKolory.textContent = 'Pole wymagane i wiekszę od 0';
+    } else {
+        uwagaKolory.textContent = '';
+    }
+});
+
+menustepi.addEventListener('input', (e) => {
+    if (e.target.value < 0) {
+        uwagastepi.textContent = 'Pole nie wymagane ,ale wiekszę od 0';
+    } else {
+        uwagastepi.textContent = '';
+    }
+});
+
+*/
+
+
+
+
+let init = () => {
+    console.log("init");
+    if (gameSteps > 0) {
+        document.getElementById('status').textContent = ` ${gameSteps} steps left`;
+    } else {
+        gameSteps = -1;
+        document.getElementById('status').textContent = `steps: unlimited`;
+    }
+
+    const move = document.getElementById('game-move');
+
+    for (let i = 0; i < gameSize; i++) {
+        let kwadrat = document.createElement('div');
+        kwadrat.className = 'kwadrat';
+        kwadrat.dataset.key = '-1';
+        kwadrat.addEventListener('click', (e) => {
+            if (e.target.dataset.key == gameColors - 1) {
+                e.target.dataset.key = 0;
+            } else {
+                e.target.dataset.key++;
+            }
+
+            e.target.textContent = e.target.dataset.key;
+
+        });
+        move.appendChild(kwadrat);
+    }
+
+}
+
+
+
+
+
+let add = () => {
+    console.log("add");
+    if (menuSize.value > 0 && menuColors.value > 0) {
+        fetch("game", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                },
+                body: JSON.stringify({
+                    steps: menuStep.value,
+                    size: menuSize.value,
+                    colors: menuColors.value
+                })
+            })
+            .then(res => res.json())
+            .then(res => {
+                gameId = res.game;
+                gameSize = res.size;
+                gameColors = res.colors;
+                gameSteps = res.steps;
+                document.getElementById('menu').style.display = 'none';
+                document.getElementById('game').style.display = 'block';
+                init();
+            })
+
+
+
+    } else {
+        uwaga.textContent = "Data not valid";
+    }
+}
+btnStart.addEventListener('click', add);
+
+
+
+let marks = () => {
+    console.log("marks");
+    const move = document.querySelectorAll('.kwadrat');
+    let count = 0;
+    const tab = [];
+    move.forEach((element) => {
+        if (element.dataset.key == -1) {
+            ++count;
+        }
+        tab.push(element.textContent);
+    });
+
+    console.log(tab);
+
+    if (count === 0) {
+        let div = document.createElement('div');
+        div.innerHTML = `step ${++step} </br>`;
+        move.forEach((element) => {
+            element.className = 'kwadratP';
+            div.appendChild(element);
+        });
+
+        let result;
+
+
+
+        fetch("game/move", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                },
+                body: JSON.stringify({
+                    game: gameId,
+                    move: tab,
+                })
+            })
+            .then(res => res.json())
+            .then(res => {
+
+
+                gameId = res.game;
+                result = res.result;
+                let solution = res.solution;
+
+                console.log(result);
+                if (result.black == gameSize) {
+                    game.style.display = "none";
+                    document.getElementById('won').style.display = 'block';
+                } else {
+                    if (solution == 'lost') {
+                        game.style.display = "none";
+                        document.getElementById('lost').style.display = 'block';
+                    }
+                }
+                let wyn = document.createElement('div');
+                wyn.textContent = `white: ${result.white}  black: ${result.black}`
+                div.appendChild(wyn);
+                init();
+            })
+
+
+        div.className = "step";
+        przebieg.appendChild(div);
+
+
+
+    }
+}
+
+
+btnPlay.addEventListener('click', marks);
+
+document.getElementById('ponownie1').addEventListener('click', () => {
+    document.getElementById('lost').style.display = 'none';
+    document.getElementById('won').style.display = 'none';
+    document.getElementById('menu').style.display = 'block';
+    gameId = 0;
+    gameSize = 0;
+    gameColors = 0;
+    gameSteps = 0;
+    step = 0;
+
+    przebieg.textContent = '';
+    document.getElementById('status').textContent = '';
+    document.getElementById('game-move').textContent = '';
+
+
+
+
+});
+
+document.getElementById('ponownie2').addEventListener('click', () => {
+    document.getElementById('lost').style.display = 'none';
+    document.getElementById('won').style.display = 'none';
+    document.getElementById('menu').style.display = 'block';
+    gameId = 0;
+    gameSize = 0;
+    gameColors = 0;
+    gameSteps = 0;
+    step = 1;
+
+    przebieg.textContent = '';
+    document.getElementById('status').textContent = '';
+    document.getElementById('game-move').textContent = '';
+
+
+
+
+});
